509f9a1bd9c2a69871cc9983ac25f81b85c7eca1
DenebTM <deneb@screee.ee>
2024-06-19 12:12:32 +0200

    fix mimalloc script

--- exercises/sheet_12/jobs/lua_mimalloc.sh
+++ exercises/sheet_12/jobs/lua_mimalloc.sh
@@ -3,9 +3,9 @@
 # Execute job in the partition "lva" unless you have special requirements.
 #SBATCH --partition=lva
 # Name your job to be able to identify it later
-#SBATCH --job-name sheet12_lua_mimalloc_final
+#SBATCH --job-name sheet12_lua_mimalloc
 # Redirect output stream to this file
-#SBATCH --output=output12_lua_mimalloc_final.log
+#SBATCH --output=output12_lua_mimalloc.log
 # Maximum number of tasks (=processes) to start in total
 #SBATCH --ntasks=1
 # Maximum number of tasks (=processes) to start per node
@@ -15,38 +15,44 @@
 
 set -e
 
-basedir="/scratch/${USER}/sheet12_lua_mimalloc$$"
+scratchdir="/scratch/${USER}"
+basedir="$scratchdir/sheet12_lua_mimalloc$$"
 nruns=10
+
+cd "$scratchdir"
+[ ! -f lua-5.4.6.tar.gz ] && wget -c https://www.lua.org/ftp/lua-5.4.6.tar.gz
+[ ! -f fib.lua ] && wget -c https://raw.githubusercontent.com/PeterTh/perf-oriented-dev/master/lua/fib.lua
 
 mkdir -p "$basedir"
 cd "$basedir"
 
 (
-	module load gcc
-	git clone https://github.com/microsoft/mimalloc
-	cd mimalloc
-	mkdir -p out/release
-	cd out/release
-	cmake ../..
-	make
+    cd "$scratchdir"
+    [ ! -d mimalloc ] && git clone https://github.com/microsoft/mimalloc
+
+    module load gcc
+    cd mimalloc
+    mkdir -p out/release
+    cd out/release
+    cmake -DCMAKE_BUILD_TYPE=Release ../..
+    make -j$(nproc)
 )
 
 (
     module load gcc
-    [ ! -f lua-5.4.6.tar.gz ] && wget -c https://www.lua.org/ftp/lua-5.4.6.tar.gz
-    tar xvf lua-5.4.6.tar.gz
-    sed -i 's/MYCFLAGS=/MYCFLAGS= -lmimalloc/' lua-5.4.6/src/Makefile
+    tar xvf "$scratchdir/lua-5.4.6.tar.gz"
     cd lua-5.4.6/ 
     make -j$(nproc)
     cp src/lua "$basedir"
 )
 
-[ ! -f fib.lua ] && wget -c https://raw.githubusercontent.com/PeterTh/perf-oriented-dev/master/lua/fib.lua
 (
     echo 'func,time'
     for run in $(seq 1 $nruns); do
         >&2 echo "Run $run of $nruns"
-        ./lua fib.lua | awk '{ print $3 "," $5 }'
+
+        LD_PRELOAD="$scratchdir/mimalloc/out/release/libmimalloc.so" \
+          ./lua "$scratchdir/fib.lua" | awk '{ print $3 "," $5 }'
     done
 ) > results_raw.csv
 
